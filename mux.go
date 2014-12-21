9d1466670a48b1ae7237a4f75e934923aa18c532
Peter Hellberg <peter@c7.se>
2014-12-21 03:28:39 +0100

    Set Content-Type for assets

--- mux.go
+++ mux.go
@@ -29,7 +29,7 @@ func staticFileRouter() *mux.Router {
 		r.PathPrefix(p).Handler(RewriteURL("index.html", fh))
 	}
 
-	r.Handle("/", http.RedirectHandler("/search/syntax/", 302))
+	r.Handle("/", http.RedirectHandler("/search/", 302))
 
 	return r
 }
@@ -37,15 +37,16 @@ func staticFileRouter() *mux.Router {
 type tpbFileHandler struct{}
 
 func (fh tpbFileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Path
+	data, err := Asset(r.URL.Path)
 
-	w.Header().Add("Content-Type", mime.TypeByExtension(filepath.Ext(name)))
-
-	data, err := Asset(name)
-
-	if err == nil {
-		w.Write(data)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
 	}
+
+	w.Header().Add("Content-Type", mime.TypeByExtension(filepath.Ext(r.URL.Path)))
+	w.Header().Set("Server", "TPB Search/"+buildCommit)
+	w.Write(data)
 }
 
 func RewriteURL(to string, h http.Handler) http.Handler {
